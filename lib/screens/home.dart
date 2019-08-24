62360cda405c2bb26655aa39f2c9cd7202e32a28
piyawatbeer <piyawatbeer@gmail.com>
2019-08-24 16:37:23 +0700

    finish 24/08/62

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,26 +9,94 @@ class _HomeState extends State<Home> {
   // Expliclt
 
   // Method
+
+  Widget mySizebox() {
+    return SizedBox(
+      width: 20.0,
+      height: 20.0,
+    );
+  }
+
+  Widget signInButton() {
+    return Expanded(
+      child: RaisedButton(
+        color: Colors.blue.shade700,
+        child: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget signUpButton() {
+    return Expanded(
+      child: OutlineButton(
+        child: Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.blue.shade700),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+      child: Row(
+        children: <Widget>[
+          signInButton(),
+          mySizebox(),
+          signUpButton(),
+        ],
+      ),
+    );
+  }
+
   Widget showLogo() {
-    return Container(alignment: Alignment.center, child: Container(
-      width: 200.0,
-      height: 550.0,
-      child: Image.asset('images/logo.png'),
-    ),);
+    return Container(
+      alignment: Alignment.center,
+      child: Container(
+        width: 150.0,
+        height: 150.0,
+        child: Image.asset('images/logo.png'),
+      ),
+    );
   }
 
   Widget showAppName() {
-    return Text('BEER UBRU');
+    return Text(
+      'BEER UBRU งงเลย',
+      style: TextStyle(
+        fontSize: 35.0,
+        fontWeight: FontWeight.bold,
+        fontStyle: FontStyle.italic,
+        color: Colors.red[200],
+        fontFamily: 'Kanit',
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          showLogo(),
-          showAppName(),
-        ]));
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: RadialGradient(
+        colors: [Colors.white, Colors.red],radius: 1.5,
+        center: Alignment.center,
+      )),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            showLogo(),
+            mySizebox(),
+            showAppName(),
+            mySizebox(),
+            showButton(),
+          ]),
+    ));
   }
 }
