d0f7a2ec7b7467687624ac72ce710a1652ca3ce4
piyawatbeer <piyawatbeer@gmail.com>
2019-08-26 16:34:42 +0700

    start Listview from firestore

---
+++ lib/models/product_model.dart
@@ -0,0 +1,7 @@
+class ProductModel {
+  // Field
+  String name, detail, path, qrCode;
+
+  //Constructor
+  ProductModel(this.name,this.detail,this.path,this.qrCode);
+}
--- lib/screens/list_product.dart
+++ lib/screens/list_product.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:beer_ubru/models/product_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -12,6 +13,8 @@ class _ListProductState extends State<ListProduct> {
   // Explicit
   // List<String> names = ['aa', 'bb', 'cc'];
   List<DocumentSnapshot> snapshots;
+  List<ProductModel> productModels = [];
+
   //Method
   @override
   void initState() {
@@ -28,14 +31,30 @@ class _ListProductState extends State<ListProduct> {
       snapshots = dataSnapshot.documents;
       for (var mySnapshot in snapshots) {
         String name = mySnapshot.data['Name'];
+        String detail = mySnapshot.data['Detail'];
+        String path = mySnapshot.data['Path'];
+        String qrCode = mySnapshot.data['QRcode'];
         print('name = $name');
-        
+
+        ProductModel productModel = ProductModel(name, detail, path, qrCode);
+       setState(() {
+          productModels.add(productModel);
+       });
       }
     });
   }
 
+  Widget showListViewProduct() {
+    return ListView.builder(
+      itemCount: productModels.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Text(productModels[index].name);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('This is ListProduct');
+    return showListViewProduct();
   }
 }
